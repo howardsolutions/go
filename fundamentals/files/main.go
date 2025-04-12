180a2b264c956a81b9f793cfe1d3c5fce8e88b11
howardsolutions <phungthinh0946519356@gmail.com>
2025-04-12 13:00:57 -0600

    Refactor file reading logic in main.go to use a variable for file path and add error handling to write output to a new file on panic.

--- fundamentals/files/main.go
+++ fundamentals/files/main.go
@@ -8,11 +8,14 @@ import (
 
 func main() {
 	rootPath, _ := os.Getwd()
+	filePath := rootPath + "/data/text.txt"
 
-	content, err := fileutils.ReadTextFile(rootPath + "/data/text.txt")
+	content, err := fileutils.ReadTextFile(filePath)
 
 	if err != nil {
 		fmt.Printf("ERROR PANIC!! %v", err)
+		newContent := fmt.Sprintf("Orignal: %v\n Double Original: %v%v", content, content, content)
+		fileutils.WriteToFile(filePath+".output.txt", newContent)
 	} else {
 		fmt.Println(content)
 	}
