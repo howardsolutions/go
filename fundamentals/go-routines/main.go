377e9cc52fdf5412dc5dbe0f8389e28e7d8c5532
howardsolutions <phungthinh0946519356@gmail.com>
2025-04-13 12:53:45 -0600

    go channel

--- fundamentals/go-routines/main.go
+++ fundamentals/go-routines/main.go
@@ -5,14 +5,21 @@ import (
 	"time"
 )
 
-func printMessage(text string) {
+func printMessage(text string, channel chan string) {
 	for i := 0; i < 10; i++ {
 		fmt.Println(text)
 		time.Sleep(200 * time.Millisecond)
 	}
+
+	channel <- "Done!"
 }
 
 func main() {
-	go printMessage("Go is awesome!")
-	printMessage("we miss classes")
+	var channel chan string
+
+	go printMessage("Go is awesome!", channel)
+	// printMessage("we miss classes")
+	response := <-channel
+
+	fmt.Println(response)
 }
