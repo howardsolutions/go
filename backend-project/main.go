a1b98f76f6575891ea4fd79fec1080136f13d6dd
howardsolutions <phungthinh0946519356@gmail.com>
2025-04-17 10:59:25 -0600

    command line flags

--- backend-project/main.go
+++ backend-project/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"howardsolutions/go/backend-project/internal/app"
 	"net/http"
@@ -8,6 +9,10 @@ import (
 )
 
 func main() {
+	var port int
+	flag.IntVar(&port, "port", 8080, "Go backend server port")
+	flag.Parse()
+
 	app, err := app.NewApplication()
 
 	if err != nil {
@@ -19,11 +24,13 @@ func main() {
 	http.HandleFunc("/health", HealthCheck)
 
 	server := &http.Server{
-		Addr:         ":8080",
+		Addr:         fmt.Sprintf("%d", port),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
+
+	app.Logger.Printf("We are running on port %d \n", port)
 
 	err = server.ListenAndServe()
 
