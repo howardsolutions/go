635c327288714081008f55fa4f9e0f314393685b
howardsolutions <phungthinh0946519356@gmail.com>
2025-04-17 11:30:30 -0600

    Api route handlers

--- backend-project/internal/app/app.go
+++ backend-project/internal/app/app.go
@@ -2,21 +2,29 @@ package app
 
 import (
 	"fmt"
+	"howardsolutions/go/backend-project/internal/api"
 	"log"
 	"net/http"
 	"os"
 )
 
 type Application struct {
-	Logger *log.Logger
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
 }
 
 // Here we want to modifying the application and using it, that's why we need to return pointer to Application (struct)
 func NewApplication() (*Application, error) {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
+	// Store will go here
+
+	// Handlers
+	workoutHandler := api.NewWorkoutHandler()
+
 	app := &Application{
-		Logger: logger,
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
 	}
 
 	return app, nil
